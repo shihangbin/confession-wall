ba29c48efad410f3604ac90856a36b06b4936cf2
三九 <18213331550@163.com>
2023-12-09 12:22:57 +0800

    feat: 审核修改

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,7 +1,7 @@
 let BASE_URL = ''
 if (import.meta.env.PROD) {
   // 生产环境
-  BASE_URL = 'https://59.110.160.132:51011'
+  BASE_URL = 'https://api.xbin.cn'
 } else {
   // 开发环境
   // BASE_URL = 'http://localhost:51011'
