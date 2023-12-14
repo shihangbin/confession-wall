b3967e3a3080eb65689985ec37e3101e9084c4d2
三九 <18213331550@163.com>
2023-12-14 14:00:35 +0800

    feat: article

--- src/pages-publish/article/article.vue
+++ src/pages-publish/article/article.vue
@@ -6,31 +6,106 @@
 
   const articleStore = useArticleStore()
   const { articleList } = storeToRefs(articleStore)
+
   const content = ref('')
   const fileList: any = ref([])
   const imgArray: any = ref([])
   const imgI: any = ref([])
+  const errcode: any = ref(0)
+
+  uni.request({
+    url: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxb4609bd47f86f85d&secret=a8335f08c978c7890fc9a099b1c616de`,
+    success: (res: any) => {
+      uni.setStorageSync('accessToken', res.data.access_token)
+    },
+  })
 
   const upImages = async (imgArray: any) => {
     uni.showLoading({
       title: '上传中...',
       mask: true,
     })
-    for (const item of imgArray) {
+    // 在一个 async 函数中使用 await
+    const uploadImage = (item: any) => {
+      const accessToken = uni.getStorageSync('accessToken')
+
+      uni.uploadFile({
+        url: `https://api.weixin.qq.com/wxa/img_sec_check?access_token=${accessToken}`,
+        method: 'POST',
+        filePath: item,
+        name: 'media', // 服务端接收文件的字段名
+        success: (res: any) => {
+          const result = JSON.parse(res.data) // 将返回的 JSON 字符串转换为对象
+          errcode.value = result.errcode
+        },
+        fail: (res) => {
+          console.error(res)
+          uni.hideLoading()
+        },
+      })
+    }
+
+    const pushImg = async (item: any) => {
+      console.log(errcode.value)
+      if (errcode.value !== 0) {
+        showToastError('none', '有违规信息')
+        return
+      }
       const token = uni.getStorageSync('token')
-      imgI.value.push(
-        await uni.uploadFile({
-          url: 'https://api.xbin.cn/article/images', // 仅为示例，非真实的接口地址
-          filePath: item,
-          name: 'file',
-          header: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-      )
+
+      const a = await uni.uploadFile({
+        url: 'https://api.xbin.cn/article/images', // 仅为示例，非真实的接口地址
+        filePath: item,
+        name: 'file',
+        header: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      let res = JSON.parse(a.data)
+
+      if (res.code == 0 && errcode.value == 0) {
+        showToastError('none', '发布成功')
+
+        setTimeout(async () => {
+          uni.switchTab({
+            url: '/pages/index/index',
+          })
+          uni.hideLoading()
+          articleList.value = []
+          await articleStore.getArticleListAction(0, 5)
+        }, 500)
+      }
+    }
+
+    for (const item of imgArray) {
+      imgI.value.push(uploadImage(item), await pushImg(item))
     }
   }
   const publishArticle = async () => {
+    const accessToken = uni.getStorageSync('accessToken')
+
+    uni.request({
+      url: `https://api.weixin.qq.com/wxa/msg_sec_check?access_token=${accessToken}`, // 请确保 accessToken 是有效的
+      method: 'POST',
+      header: {
+        'content-type': 'application/json', // 设置请求头为 JSON
+      },
+      data: {
+        content: content.value,
+      },
+      success: (res: any) => {
+        errcode.value = res.data.errcode
+      },
+      fail: (err) => {
+        console.error('Request failed:', err)
+      },
+    })
+
+    if (errcode.value !== 0) {
+      showToastError('none', '有违规信息')
+      return
+    }
+
     if (content.value.length < 2) {
       showToastError('none', '字数少于二')
       return
